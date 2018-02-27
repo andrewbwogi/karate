686821e465a462e5f073b3a0fc4a0926916cae30
andrewbwogi <bwogi.andrew@gmail.com>
2018-02-27 11:06:53 +0100

    Revert "Fix CoverageStructure bugs"
    This reverts commit cebd35cc60e849b829ed354a402e1ae426619b4e.

--- karate-core/src/main/java/com/intuit/karate/CoverageStructure.java
+++ karate-core/src/main/java/com/intuit/karate/CoverageStructure.java
@@ -2,26 +2,18 @@ package com.intuit.karate;
 
 import java.util.ArrayList;
 
-/**
- * Usage:
- * 1. Call CoverageStructure.setCoverageStructure() in the beginning of the test function.
- * 2. Set out CoverageStructure.addBranch() in every branch of the tested function.
- * 3. Call CoverageStructure.printBranches() in the end of the test function.
- */
 public class CoverageStructure {
 
-    public static String functionName;
-    public static int noOfBranches;
-    public static ArrayList<Integer> branches; // array containing id's of each branch
+    public String functionName;
+    public int noOfBranches;
+    public ArrayList<Integer> branches; // array containing id's of each branch
 
-    public static void setCoverageStructure(String functionNameIn, int noOfBranchesIn) {
-        functionName = functionNameIn;
-        noOfBranches = noOfBranchesIn;
-        branches = new ArrayList<>();
-        System.out.println("*********Now covering: " + functionName + "*********");
+    CoverageStructure(String functionName, int noOfBranches) {
+        this.functionName = functionName;
+        this.noOfBranches = noOfBranches;
     }
 
-    public static int addBranch(int id) {
+    public int addBranch(int id) {
         if(!branches.contains(id)) {
             branches.add(id);
             return 1;
@@ -30,16 +22,13 @@ public class CoverageStructure {
         }
     }
 
-    public static void printBranches() {
+    public void printBranches() {
         System.out.printf("Reached %d out of %d branches.\n", branches.size(), noOfBranches);
-        if(noOfBranches != 0)
-            System.out.println("Coverage: " + (branches.size()/noOfBranches) + "%\n");
-        else
-            System.out.println("No Branches");
+        System.out.println("Coverage: " + (branches.size()/noOfBranches) + "%");
         System.out.println("===Showing reached branches===");
         for(Integer id : branches)
             System.out.printf("Branch id %d reached.\n", id);
-        System.out.println("\n===Showing unreached branches===");
+        System.out.println("\n\n\n===Showing unreached branches===");
         for(int i = 0; i < noOfBranches; i++){
             if(!branches.contains(i))
                 System.out.printf("Branch id %d not reached.\n", i);
